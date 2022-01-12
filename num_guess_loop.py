0337443a358d13400a0076f2304a464a53f7e9db
Ubuntu <ubuntu@ip-172-31-86-174.ec2.internal>
2022-01-12 17:03:09 +0000

    Updated the number guessing loop program

--- num_guess_loop.py
+++ num_guess_loop.py
@@ -3,17 +3,18 @@
 # Created by: Ketia Gaelle Kaze
 # Created on: Jan. 11, 2022
 # This program asks the user to guess a number between
-# 0 and 9 and then uses a loop to check if the guessed number 
+# 0 and 9 and then uses a loop to check if the guessed number
 # is equal to the random number generated
 
 import random
+
 
 def main():
     # initialize the loop counter and the random number
     loop_counter = 0
     # generate random number
     random_number = random.randint(0, 9)
-    
+
     while True:
         # get the guessed number as string
         guessed_number_string = input("Guess a number between 0 and 9: ")
@@ -29,16 +30,20 @@ def main():
                     print("Your guess is correct. Thanks for playing.")
                     break
                 else:
-                    #Display the result
-                    print("Your guess is wrong. The correct number is {}".format(random_number))
+                    # Display the result
+                    print("Your guess is wrong. The correct number is {}"
+                          .format(random_number))
                     print("")
-                    print("Tracking {} times through the loop.".format(loop_counter))
+                    print("Tracking {} times through the loop."
+                          .format(loop_counter))
                     print("")
             else:
-                print("{} is not in the range of numbers to guess from. Please try again.".format(guessed_number))
+                print("{} is not in the range of numbers to guess from."
+                      "Please try again.".format(guessed_number))
         except Exception:
-            print("{} is not a number. Please try again.".format(guessed_number_string))
+            print("{} is not a number. Please try again."
+                  .format(guessed_number_string))
 
 
 if __name__ == "__main__":
-      main()
+    main()
